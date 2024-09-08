f7dd3691b7ac31214327433bdc1151a37d2cf6a6
Thanawat Tantijaroensin <taroofsatoeimo@gmail.com>
2024-09-08 21:01:24 +0700

    add logging when user failed to login

--- polls/signals.py
+++ polls/signals.py
@@ -1,9 +1,10 @@
 # polls/signals.py
 
-from django.contrib.auth.signals import user_logged_in, user_logged_out
+from django.contrib.auth.signals import user_logged_in, user_logged_out, user_login_failed
 from django.dispatch import receiver
 import logging
 from .utils import get_client_ip
+
 
 logger = logging.getLogger('myapp')
 
@@ -16,4 +17,12 @@ def log_user_login(sender, request, user, **kwargs):
 
 @receiver(user_logged_out)
 def log_user_logout(sender, request, user, **kwargs):
-    logger.info(f"User logged out: {user.username}")
+    ip_address = get_client_ip(request)
+    logger.info(f"User {user.username} logged out from IP address {ip_address}.")
+
+
+@receiver(user_login_failed)
+def log_user_login_failed(sender, credentials, request, **kwargs):
+    ip_address = get_client_ip(request)
+    username = credentials.get('username', 'Unknown')
+    logger.warning(f"Login failed for username {username} from IP address {ip_address}.")
