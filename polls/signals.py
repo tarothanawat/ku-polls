e06f9fccf63f4cce3094bc2237ad21ceafa5e60c
Thanawat Tantijaroensin <taroofsatoeimo@gmail.com>
2024-09-08 20:52:28 +0700

    User IP address is logged when they login

--- polls/signals.py
+++ polls/signals.py
@@ -3,13 +3,15 @@
 from django.contrib.auth.signals import user_logged_in, user_logged_out
 from django.dispatch import receiver
 import logging
+from .utils import get_client_ip
 
 logger = logging.getLogger('myapp')
 
 
 @receiver(user_logged_in)
 def log_user_login(sender, request, user, **kwargs):
-    logger.info(f"User logged in: {user.username}")
+    ip_address = get_client_ip(request)
+    logger.info(f"User {user.username} logged in from IP address {ip_address}.")
 
 
 @receiver(user_logged_out)
---
+++ polls/utils.py
@@ -0,0 +1,13 @@
+def get_client_ip(request):
+    """Get the visitor’s IP address using request headers."""
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0].strip()
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+
+    # Additional security measure to ensure the IP is in valid format
+    if not ip:
+        ip = 'Unknown'
+
+    return ip
