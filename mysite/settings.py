e9ade3a3d669c0c8b8b0a752a79ec7d47c6e15b3
Thanawat Tantijaroensin <taroofsatoeimo@gmail.com>
2024-09-08 19:29:07 +0700

    The choice that user previously select now shows a radio pressed radio button

--- mysite/settings.py
+++ mysite/settings.py
@@ -133,3 +133,23 @@ STATIC_URL = 'polls/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# LOGGING
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'django_debug.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
--- polls/views.py
+++ polls/views.py
@@ -8,8 +8,6 @@ from .models import Choice, Question, Vote
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-
-
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
@@ -22,23 +20,61 @@ class IndexView(generic.ListView):
         return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
 
 
-class DetailView(LoginRequiredMixin, generic.DetailView):
+class DetailView(generic.DetailView):
+    """
+    Displays the choices for a poll and allow voting.
+
+    Attributes:
+        model (Question): The model associated with this view.
+        template_name (str): The path to the template that renders the view.
+    """
+
     model = Question
-    template_name = "polls/detail.html"
+    template_name = 'polls/detail.html'
 
     def get_queryset(self):
-        """
-        Excludes any questions that aren't published yet.
-        """
+        """Excludes any questions that aren't published yet."""
         return Question.objects.filter(pub_date__lte=timezone.now())
 
     def get(self, request, *args, **kwargs):
-        question = self.get_object()
-        if not question.can_vote():
-            messages.error(request, "Voting is not allowed for this poll.")
-            return redirect(reverse('polls:index'))
+        """
+        Get the question object and render the template.
 
-        return super().get(request, *args, **kwargs)
+        Args:
+            request: The HTTP request object.
+            self.object (int): The ID of the question being voted on.
+
+        Returns:
+            HttpResponseRedirect: Redirects to the index page with an error
+                                  message if the question is not published.
+            HttpResponse: Renders the results page.
+        """
+        # Get the question object
+        self.object = self.get_object()
+
+        # Check if voting is allowed
+        if not self.object.can_vote():
+            messages.error(request, "This poll is closed.")
+            return redirect('polls:index')
+
+        return self.render_to_response(self.get_context_data(
+                                        object=self.object))
+
+    def get_context_data(self, **kwargs):
+        """Adds the previous choice of the user to the context data."""
+        context = super().get_context_data(**kwargs)
+        question = self.object
+        this_user = self.request.user
+
+        if this_user.is_authenticated:
+            try:
+                previous_vote = Vote.objects.get(user=this_user, choice__question=question)
+                context['previous_choice'] = previous_vote.choice
+            except Vote.DoesNotExist:
+                context['previous_choice'] = None
+        else:
+            context['previous_choice'] = None
+        return context
 
 
 class ResultsView(generic.DetailView):
@@ -70,28 +106,14 @@ class VoteView(LoginRequiredMixin, View):
 
         choice_id = request.POST.get("choice")
         if not choice_id:
-            # Handle the case where no choice is selected
-            return render(
-                request,
-                "polls/detail.html",
-                {
-                    "question": question,
-                    "error_message": "You didn't select a choice.",
-                },
-            )
+            messages.error(request, "You didn't select a choice.")
+            return HttpResponseRedirect(reverse('polls:detail', args=(question.id,)))
 
         try:
             selected_choice = question.choice_set.get(pk=choice_id)
         except Choice.DoesNotExist:
-            # Handle the case where the choice is invalid
-            return render(
-                request,
-                "polls/detail.html",
-                {
-                    "question": question,
-                    "error_message": "Invalid choice.",
-                },
-            )
+            messages.error(request, "Invalid choice.")
+            return HttpResponseRedirect(reverse('polls:detail', args=(question.id,)))
 
         existing_vote = Vote.objects.filter(user=user, choice__question=question).first()
         if existing_vote:
