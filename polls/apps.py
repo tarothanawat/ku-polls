4c4cf7d59a7ab7e0921e4c260617c0c5f71476d6
Thanawat Tantijaroensin <taroofsatoeimo@gmail.com>
2024-09-14 17:25:02 +0700

    flake8 +4

--- polls/apps.py
+++ polls/apps.py
@@ -4,15 +4,11 @@ from django.apps import AppConfig
 
 
 class PollsConfig(AppConfig):
-    """
-    Configuration class for the 'polls' app.
-    """
+    """Configuration class for the 'polls' app."""
 
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'polls'
 
     def ready(self):
-        """
-        Import signal handlers when the app is ready.
-        """
+        """Import signal handlers when the app is ready."""
         import polls.signals
